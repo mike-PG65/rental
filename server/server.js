73899c4eb57cc858c1365a67daa4826700408c90
mike-PG65 <petergonye123@gmail.com>
2025-11-10 16:29:42 +0300

    added the frontend url in the server file

--- server/server.js
+++ server/server.js
@@ -22,7 +22,8 @@ const corsOptions = {
   origin: [
     "http://localhost:5175",
     "https://tenant-chi.vercel.app",
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "https://rental-beryl-tau.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
